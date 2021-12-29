c5665cc2b08ced57bedeba13897e7f3baf9f0b24
caroccucurullo <caroccucurullo@gmail.com>
2021-12-28 22:03:29 -0300

    Desafío 9: Cart View

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,17 +1,74 @@
-import React from 'react'
-import { useCartContext } from "./CartContext"
+import { Table, Container, Row, Col, Button } from 'react-bootstrap';
+import { useCartContext } from './CartContext';
+import { Link } from 'react-router-dom';
 
-function Cart() {
+const Cart = () => {
+  const { borrarItem, borrarCarrito, total, cartList } = useCartContext();
 
-    const {  cartList, borrarCarrito  } = useCartContext()
-
+  if (total() === 0) {
     return (
-        <div>
-             {  cartList.map(prod=> <li>{prod.nombre}   {prod.cantidad}</li>) }
-            <button onClick={borrarCarrito} >Vaciar Carrito</button>
+      <Container className="mt-5 mb-5">
+        <Row style={{ margin: '0 auto' }}>
+          <Col
+            md={6}
+            className="d-flex flex-column justify-content-center align-items-center align-content-center m-auto"
+          >
+            <h4>Agregá MOBIs a tu carrito</h4>
+            <Link to={'/'}>
+              <button>Ir a la página principal</button>
+            </Link>
+          </Col>
+        </Row>
+      </Container>
+    );
+  } else {
+    return (
+      <>
+        <Container style={{ marginTop: '20px' }}>
+          <Table striped bordered hover responsive>
+            <thead>
+              <tr>
+                <th></th>
+                <th>Nombre</th>
+                <th>Precio</th>
+                <th>Cantidad</th>
+                <th>Total</th>
+                <th></th>
+              </tr>
+            </thead>
+            <tbody>
+              {cartList.map((c) => (
+                <tr key={c.id}>
+                  <td>
+                    <img src={c.img} alt="producto"/>
+                  </td>
+                  <td>{c.nombre}</td>
+                  <td>{c.precio}</td>
+                  <td>{c.cantidad}</td>
+                  <td>{c.precio * c.cantidad}</td>
+                  <td>
+                    <Button onClick={() => borrarItem(c.id)}>
+                     Eliminar</Button>
+                  </td>
+                </tr>
+              ))}
+            </tbody>
+          </Table>
+        </Container>
+        <Container>
+          <Row>
+            <Col className="d-flex flex-row justify-content-end mb-5">
+              <h4>Total ${total()}.-</h4>
+              <button onClick={borrarCarrito}>
+                Limpiar carrito
+              </button>
+            </Col>
+          </Row>
+        </Container>
+        
+      </>
+    );
+  }
+};
 
-        </div>
-    )
-}
-
-export default Cart
+export default Cart;
--- src/components/CartContext.js
+++ src/components/CartContext.js
@@ -8,31 +8,61 @@ export const useCartContext = () => useContext(CartContext)
 function CartContextProvider({children}) {
     const [cartList, setCartList] = useState([])
 
-function agregarAlCarrito(item) {       
+    function agregarAlCarrito(item) {       
+        const index = cartList.findIndex(i => i.id === item.id)
+    
+            if (index > -1) {
+                const oldItem = cartList[index].cantidad
+    
+                cartList.splice(index, 1)
 
-    const index = cartList.findIndex(i => i.id === item.id)
+                setCartList([...cartList, { ...item, cantidad: item.cantidad + oldItem}])
+
+                sumarCantidad(item);
+
+            } else {
+                setCartList([...cartList, item])
+            }
+    }
+
+    const sumarCantidad = (counter, item) => {
+        const cantidad = [...cartList];
+        cantidad.forEach((c) => {
+        c.id === item.id && (c.cantidad += counter);
+        });
+
+        setCartList(cantidad);
+    };
+
+    const total = () => {
+        const sumaTotal = cartList.reduce((x, y) => x + y.precio * y.cantidad, 0);
+        return sumaTotal;
+    };
+
   
-          if (index > -1) {
-            const oldItem = cartList[index].cantidad
-  
-            cartList.splice(index, 1)
+    const unidades = () => {
+        const numeros = cartList.reduce((x, y) => x + y.cantidad, 0);
+        return numeros;
+    };
 
-            setCartList([...cartList, { ...item, cantidad: item.cantidad + oldItem}])
 
-          } else {
-            setCartList([...cartList, item])
-          }
-      }
+    const borrarItem = (item) => {
+        const itemRemoved = cartList.filter((i) => i.id !== item);
+        setCartList(itemRemoved);
+    };
 
-function borrarCarrito() {
-        setCartList([])
+    function borrarCarrito() {
+            setCartList([])
     }
     
 return (
         <CartContext.Provider value={{
             cartList,
             agregarAlCarrito,
-            borrarCarrito
+            borrarCarrito,
+            borrarItem,
+            total,
+            unidades,
         }}>
             { children }
         </CartContext.Provider>
